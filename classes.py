12753b41054596ea0abbb6e1aceab307bdd2231b
URAHOV <62206245+URAHOV@users.noreply.github.com>
2020-07-11 19:23:28 +0500

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+class Person:
+
+    def __init__(self, name='', age=None):
+        self.name = name
+        self.age = age
+    
+    def get_age(self):
+        your_age = str(self.age)
+        return your_age
+
+## or'5'or'6'or'7'or'8'or'9'
