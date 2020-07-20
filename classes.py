177526e0651f28043caf116759fde0880d044e06
URAHOV <62206245+URAHOV@users.noreply.github.com>
2020-07-20 13:42:08 +0500

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,10 +1,11 @@
 
 class Person:
 
-    def __init__(self, name='', age=None):
+    def __init__(self, name='', age=None): ## конструктор класса
         self.name = name
         self.age = age
     
-    def get_age(self):
+    def get_person_data(self):
+        your_name = str(self.name)
         your_age = str(self.age)
-        return your_age
+        return (your_name, your_age) ## получаем кортеж
